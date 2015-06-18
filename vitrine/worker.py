c9b4468b1c50132ccb0021d70baac836a96bba14
jonathansp <jonathan.simonprates@gmail.com>
2015-06-18 19:29:33 -0300

    Change config vars

--- vitrine/worker.py
+++ vitrine/worker.py
@@ -52,7 +52,8 @@ class LangStatsWorker(Shepherd):
 class CommitWorker(Shepherd):
 
     def initialize(self):
-        mongoengine.connect(self.config.DB_NAME, host=self.config.DB_HOST)
+        mongoengine.connect(
+            self.config.MONGODB_DB, host=self.config.MONGODB_HOST)
 
     def get_description(self):
         return 'Commit worker {}'.format(__version__)
@@ -63,26 +64,27 @@ class CommitWorker(Shepherd):
         gitlab = Gitlab(self.config.GITLAB_URL, self.config.GITLAB_TOKEN)
         gitlab.auth()
 
-        logging.debug('Loading projects from gitlab...')
+        logging.info('Loading projects from gitlab...')
 
         for project in gitlab.Project():
             for cmt in project.Commit():
 
                 try:
                     commit = Commit.objects.get(commit_id=cmt.id)
+
+                    # Commit already exists.
                 except:
                     commit = Commit()
-
-                commit.commit_id = cmt.id
-                commit.short_id = cmt.short_id
-                commit.title = cmt.title
-                commit.author_name = cmt.author_name
-                commit.author_email = cmt.author_email
-                commit.created_at = parser.parse(cmt.created_at)
-                commit.team_name = "team"
-                commit.project_id = cmt.project_id
-                commit.project_name = project.name
-                commit.save()
+                    commit.commit_id = cmt.id
+                    commit.short_id = cmt.short_id
+                    commit.title = cmt.title
+                    commit.author_name = cmt.author_name
+                    commit.author_email = cmt.author_email
+                    commit.created_at = parser.parse(cmt.created_at)
+                    commit.team_name = "team"
+                    commit.project_id = cmt.project_id
+                    commit.project_name = project.name
+                    commit.save()
 
 
 def main():
@@ -93,8 +95,6 @@ def main():
 def commit():
     worker = CommitWorker(sys.argv[1:])
     worker.run()
-commit()
-
 
 def langstats():
     worker = LangStatsWorker(sys.argv[1:])
