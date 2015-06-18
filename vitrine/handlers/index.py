bf6df5e9c089090256b62bfb9da60892ae3a79d7
Glaucia Peres <gauperes@gmail.com>
2015-06-18 19:27:36 -0300

    add function to get users by group

--- vitrine/handlers/index.py
+++ vitrine/handlers/index.py
@@ -10,15 +10,30 @@
 
 from datetime import datetime
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 from vitrine.models.user import User
+
+import gitlab
 
 
 mod = Blueprint('index', __name__)
 
 
+def get_group_users(group_id):
+    gl = gitlab.Gitlab(current_app.config.get('GITLAB_BASE_URL'), current_app.config.get('APP_SECRET_KEY'))
+    gl.auth()
+
+    members = gl.Group(group_id).Member()
+    users = []
+
+    for member in members:
+        users.append(gl.User(member.id))
+
+    return users
+
+
 @mod.route("/")
 def index():
-    users = User.objects.all()
+    users = User.objects.all()a
     return render_template('index.html', dt=datetime.now().strftime("%d %M %Y - %H %m %s"), users=users)
